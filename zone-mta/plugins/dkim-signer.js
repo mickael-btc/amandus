081a3ee11121e3813175407e29826711035025fa
mickael <mickael@the-hype.store>
2021-12-22 22:16:21 +0100

    added zone-mta to project

---
+++ zone-mta/plugins/dkim-signer.js
@@ -0,0 +1,37 @@
+//@ts-check
+"use strict";
+
+// DKIM PLUGIN MANAGER
+
+const axios = require("axios").default;
+
+module.exports.title = "DKIM Plugin Manager";
+
+module.exports.init = (app, done) => {
+  app.addHook("sender:connection", async (delivery, connection, next) => {
+    // make sure that there is a key array present
+    if (!delivery.dkim.keys) {
+      delivery.dkim.keys = [];
+    }
+
+    // resolve the domain name of the sender
+    const fromDomain = (delivery.from || "localhost").split("@").pop().toLowerCase();
+
+    try {
+      const response = await axios.get(`http://localhost:8513/dkim/current/${fromDomain}`);
+      // push all signature keys to the key array
+      delivery.dkim.keys.push({
+        domainName: response.data.domain,
+        keySelector: response.data.selector,
+        privateKey: response.data.privateKey,
+      });
+ 
+      app.logger.info("DKIM", "%s.%s Added DKIM key for %s <%s>", delivery.id, delivery.seq, fromDomain, delivery.messageId);
+    } catch (error) {
+      app.logger.error("DKIM", "%s.%s Not Added DKIM key for %s <%s>", delivery.id, delivery.seq, fromDomain, delivery.messageId);
+    } finally {
+      next();
+    }
+  });
+  done();
+};
---
+++ zone-mta/plugins/header-patcher.js
@@ -0,0 +1,21 @@
+//@ts-check
+require("dotenv").config();
+
+module.exports.title = "Header Patch Plugin";
+
+module.exports.init = (app, done) => {
+
+  app.addHook("sender:headers", async (delivery, connection) => {
+    delivery.headers = delivery.headers || {};
+    delivery.headers.lines = delivery.headers.lines || {};
+
+    const lines = delivery.headers.lines;
+    delivery.headers.lines = lines.filter((obj) => obj["key"] !== "received");
+  });
+
+  app.addHook("sender:connect", async (delivery, options) => {
+    options["localHostname"] = process.env.SENDER_DOMAIN;
+  });
+  
+  done();
+};
